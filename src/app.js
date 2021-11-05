be1d1bdcbe8656d6ef392b59041559ce44d0ec5a
Gökçe <gokcebicici95@gmail.com>
2021-11-05 23:01:30 +0300

    5.11.2021 ders sonu

---
+++ src/app.js
@@ -0,0 +1,34 @@
+console.log("Merhaba")
+//admin script kodlama yöntemini kullanıyoruz.
+//JS type safe değil ilk başta double girdiğin değişkene daaha sonra strng girebilirsin
+var dolarBugun = 9.30
+var dolarDun = 9.20
+console.log("Dolar Bugun:"+dolarBugun)
+// var legacy yani eski kod bundan sonra kullanmayacağız
+
+let havaBugun = 17
+let havaDun = 10
+console.log(havaBugun)
+
+const euroDun=12.2
+//const a atadığın veriyi daha sonra değştiremezsin.
+
+//array
+let konutKredileri = ["ev","araba","yat","kat"]
+//camelCasing
+//PascalCasing
+// ctrl+k+c toplu yoruma alma
+// extensions -> angular snippets 12 yüklendi
+console.log("<ul>")
+for (let i = 0; i <konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+// extensions-> auto rename take
+//bracket pair colorizer2
+//dracula official   dark tema
+//editor config for vs code
+//es7/ react/redux /graphQl/React+native /js snippets
+//material icon theme
+//react code snippets ?
